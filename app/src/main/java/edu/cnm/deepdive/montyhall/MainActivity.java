709881657a66d330f86e20e466cfd80ba1b2a37a
Joseph Peterson <jscpeterson@gmail.com>
2018-10-02 14:59:47 -0600

    Merge branch 'master' of /home/joseph/Desktop/bootcamp/projects/MontyHall with conflicts.

--- app/src/main/java/edu/cnm/deepdive/montyhall/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/montyhall/MainActivity.java
@@ -3,9 +3,12 @@ package edu.cnm.deepdive.montyhall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +17,14 @@ public class MainActivity extends AppCompatActivity {
   private int wins;
   private int losses;
   private int winningDoor;
+  private int selectionID = 0;
   private ImageButton okButton;
+  private ImageButton newGameButton;
   private ImageButton door1;
   private ImageButton door2;
   private ImageButton door3;
+  private List<ImageButton> activeDoors = new ArrayList<>();
+  private ImageButton prizeDoor;
   private TextView bannerField;
   private TextView messageField;
   private TextView winTally;
@@ -42,12 +49,19 @@ public class MainActivity extends AppCompatActivity {
         //((ImageButton) v).setImageResource();
       }
     };*/
-    /*View.OnClickListener doorListener = (view) -> {
-          ((ImageButton) view).setImageResource(reddoor_selected);
-        };
+    OnClickListener doorListener = (view) -> {
+         // ((ImageButton) view).setImageResource();
+      okButton.setEnabled(true);
+    };
+    okButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    };
     door1.setOnClickListener(doorListener);
     door2.setOnClickListener(doorListener);
-    door3.setOnClickListener(doorListener);*/
+    door3.setOnClickListener(doorListener);
   }
 
   @Override
@@ -59,13 +73,30 @@ public class MainActivity extends AppCompatActivity {
   }
 
   void selectWinningSpace() {
-    winningDoor = rng.nextInt(3)+1;
+    winningDoor = rng.nextInt(3);
+    activeDoors.add(door1);
+    activeDoors.add(door2);
+    activeDoors.add(door3);
+    prizeDoor = activeDoors.remove(winningDoor);
+  }
+
+  void selectDoor(ImageButton door) {
+    if (selectionID == 0) {
+
+    } else if (selectionID != door.getId()) {
+
+    }
   }
 
   void firstDoor() {
     messageField.setText(R.string.pick_a_door);
     //OK_Button set inactive if no doors are selected
+    okButton.setEnabled(false);
     //When user selects a door, OK_Button is active
+
+    door1.setOnClickListener(doorListener);
+    door2.setOnClickListener(doorListener);
+    door3.setOnClickListener(doorListener);
     //When OK_Button is pressed, proceed to revealSecondDoor
   }
 
@@ -74,7 +105,9 @@ public class MainActivity extends AppCompatActivity {
     //if it isnt, reveal door that is not winningDoor
     //if it is winningDoor, randomly select a losing door
     //deactivate losing door
+    //door1.setEnabled(false);
     //proceed to secondDoor
+    //door1.setEnabled(false);
   }
 
   void secondDoor() {
